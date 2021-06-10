029eb0736851026047bbe8b6954ff93ea7f3f611
Mehdi Cherti <mehdicherti@gmail.com>
2021-06-10 09:33:22 +0200

    first commit of code

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+description = "Simplified tool for evaluating a DALL-e model"
+
+setup(
+    name="clip_score",
+    version="0.1.0",
+    author="Mehdi Cherti",
+    description=description,
+    license="MIT",
+    url="https://github.com/mehdidc/DALLE_clip_score",
+    zip_safe=False,  # the package can run out of an .egg file
+    classifiers=['Intended Audience :: Science/Research',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development',
+                 'Topic :: Scientific/Engineering',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX',
+                 'Operating System :: Unix',
+                 'Operating System :: MacOS'],
+    platforms='any',
+    scripts=['clip_score'],
+    include_package_data=True,
+    install_requires=['clize', 'clip@git+https://github.com/openai/CLIP'],
+)
+
